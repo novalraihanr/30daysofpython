ccc85d6acd12f39d6f21449242e3085f74fe7da4
novalraihanr <novalrr21@gmail.com>
2025-02-27 13:50:09 +0700

    day_1

---
+++ day_1/helloworld.py
@@ -0,0 +1,51 @@
+# Operation
+print(1 + 2)  # addition
+print(3 - 2)  # subtraction
+print(3 * 4)  # multiplication
+print(25 % 5)  # modulus
+print(6 / 3)  # division
+print(20**2)  # exponential
+print(50 // 3)  # flood division
+
+# printing
+print("Noval")
+print("I dont have a family name")
+print("Indonesia")
+print("I am enjoying 30 days of python")
+
+# print type of
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Asabeneh", "Python", "Finland"]))
+print(type("Noval"))
+print("I dont have a family name")
+print("Indonesia")
+
+# Data type
+n_integer = 1
+n_float = 1.9
+n_complex = complex(2 + 3j)
+
+print(type(n_integer))
+print(type(n_float))
+print(type(n_complex))
+
+n_string = "this is a string"
+n_bool = bool(False)
+d_list = [1, 2, 6, 7, 10, 12]
+d_tuple = ("apple", "banana", "pineapple", "watermelon")
+d_set = {8, 2, 9, 18, 7}
+d_dictionaty = {
+    "title": "Sherlock Holmes",
+    "author": "Arthur Conan Doyle",
+    "year": 1912,
+}
+
+print(type(n_string))
+print(type(n_bool))
+print(type(d_list))
+print(type(d_tuple))
+print(type(d_set))
+print(type(d_dictionaty))
